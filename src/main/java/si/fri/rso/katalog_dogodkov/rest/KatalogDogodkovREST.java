8962d3aaa5ec489bed775d1e261c1d68c4842ca3
kustrun <domen.balantic1@gmail.com>
2018-01-07 20:04:02 +0100

    Basic implementation of health, metrics and logs.

--- src/main/java/si/fri/rso/katalog_dogodkov/rest/KatalogDogodkovREST.java
+++ src/main/java/si/fri/rso/katalog_dogodkov/rest/KatalogDogodkovREST.java
@@ -5,6 +5,7 @@ import com.kumuluz.ee.logs.cdi.LogParams;
 import org.eclipse.microprofile.metrics.annotation.Metric;
 import si.fri.rso.katalog_dogodkov.bean.DogodekBean;
 import si.fri.rso.katalog_dogodkov.entity.Dogodek;
+import si.fri.rso.katalog_dogodkov.entity.Projekt;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -28,6 +29,22 @@ public class KatalogDogodkovREST {
     @Path("/test")
     public Response test() {
         return Response.ok("OK").build();
+    }
+
+    @GET
+    @Path("/info")
+    public Response projekt() {
+
+        Projekt projekt = new Projekt();
+        projekt.setClani(new String[]{"db8913", "jk1234"});
+        projekt.setOpisProjekta("Projekt implementira preprosto socialno omrežje");
+        projekt.setMikrostoritve(new String[]{"http://169.51.21.133:30010/v1/katalog_dogodkov", "http://169.51.21.133:30011/v1/upravljanje_dogodkov"});
+        projekt.setGithub(new String[]{"https://github.com/RSO-skupina1/katalog_dogodkov", "https://github.com/RSO-skupina1/upravljanje_dogodkov"});
+        projekt.setTravis(new String[]{"https://travis-ci.org/RSO-skupina1/katalog_dogodkov", "https://travis-ci.org/RSO-skupina1/upravljanje_dogodkov"});
+        projekt.setDockerhub(new String[]{"https://hub.docker.com/r/db8913/katalog_dogodkov", "https://hub.docker.com/r/db8913/upravljanje_dogodkov"});
+
+        return Response.ok(projekt).build();
+
     }
 
     @GET
